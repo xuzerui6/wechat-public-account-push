716492eb14e9b2c626d5db4c27ccc4ad2c5b6a6e
xuzerui6 <xuzerui6@gmail.com>
2024-10-07 11:38:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ow1PF6mf4YFEIuwi9bDeSb9ccwME',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '7KnMcBvPjIJwuRGS8H3tJsj8jpxan2E9rhKwEfHhSyo',
+      useTemplateId: 'BtiaNJo3k1KscDysYeXVQJnJFC4JReQcySLBsBnn1fU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-18',
       festivals: [
